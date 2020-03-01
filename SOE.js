ce5d3402877ebfc9a1e46addaf112a42ce4b9cde
Kerr Salabao <kerr712@gmail.com>
2020-02-29 20:56:26 -0600

    Initial files

---
+++ SOE.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+  let input = document.querySelector("input")
+
+  document.querySelector("#calculate").addEventListener("click",(e) => {
+    console.log('addition');
+    console.log('input',input1.value);
+
+
+
+
+
+  });
+
+
+
+
+
+
+});
