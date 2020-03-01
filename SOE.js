5177f842d810f914a4349f012dea946216a3655b
Kerr Salabao <kerr712@gmail.com>
2020-02-29 21:03:56 -0600

    Update SOE.js

--- SOE.js
+++ SOE.js
@@ -1,12 +1,21 @@
+function
+
+
+
+
 document.addEventListener("DOMContentLoaded", ()=>{
-  let input = document.querySelector("input")
+  let input = document.querySelector("input");
 
   document.querySelector("#calculate").addEventListener("click",(e) => {
     console.log('addition');
     console.log('input',input1.value);
 
 
-
+    let n = parseInt(input.value);
+    if(n<2 || !isNaN(n))
+    {
+      document.querySelector("#result").innerText = "Invalid input!"
+    }
 
 
   });
